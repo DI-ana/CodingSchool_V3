4ee41979dddf79f98739d764a758ba909515cf38
Diana Egiazarova <78656084+DI-ana@users.noreply.github.com>
2021-04-13 01:02:16 +0400

    Homework 3 / JS

---
+++ main.js
@@ -0,0 +1,32 @@
+const proceed = confirm("რეგისტრაცია");
+
+
+if (proceed){
+  const registration = confirm("რეგისტრაციაზე თანხმობა");
+}
+else {
+  const registration2 = confirm("უარი რეგისტრაციაზე");
+}
+
+const result = prompt(
+    `ჩაწერეთ თქვენი სახელი`
+);
+
+const result2 = prompt(
+    `ჩაწერეთ თქვენი გვარი`
+  );
+
+const result3 = prompt(
+    `ჩაწერეთ თქვენი მეილი`
+  );
+
+
+  console.log (result);
+  console.log (result2);
+  console.log (result3);
+
+ 
+  
+   
+  
+  
